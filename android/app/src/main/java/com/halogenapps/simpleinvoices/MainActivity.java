c2bbabcf9d7fec68f0ebd4de97d2840436bbc1fa
Anesu Kafesu <anesukafesu@gmail.com>
2022-07-06 20:28:09 +0200

    Configured Capacitor and added Android platform

---
+++ android/app/src/main/java/com/halogenapps/simpleinvoices/MainActivity.java
@@ -0,0 +1,5 @@
+package com.halogenapps.simpleinvoices;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.halogenapps.simpleinvoices',
+  appName: 'Simple Invoices',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
